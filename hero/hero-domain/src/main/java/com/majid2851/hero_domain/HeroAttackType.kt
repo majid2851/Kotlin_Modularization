f373f1f4cae85b0eabf9d4591a6cc3306681e618
majid2851 <majidbagheri2851@gmail.com>
2023-04-23 12:35:27 +0430

    Part 8 is Done --Hero Domain Model

---
+++ hero/hero-domain/src/main/java/com/majid2851/hero_domain/HeroAttackType.kt
@@ -0,0 +1,33 @@
+package com.majid2851.hero_domain
+
+
+sealed class HeroAttackType(
+    val uiValue: String,
+){
+
+    object Melee: HeroAttackType(
+        uiValue = "Melee",
+    )
+
+    object Ranged: HeroAttackType(
+        uiValue = "Ranged",
+    )
+
+    object Unknown: HeroAttackType(
+        uiValue = "Unknown",
+    )
+}
+
+fun getHeroAttackType(uiValue: String): HeroAttackType{
+    return when(uiValue){
+        HeroAttackType.Melee.uiValue -> {
+            HeroAttackType.Melee
+        }
+        HeroAttackType.Ranged.uiValue -> {
+            HeroAttackType.Ranged
+        }
+        else -> {
+            HeroAttackType.Unknown
+        }
+    }
+}
