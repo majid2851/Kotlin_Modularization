611a69a9248e865591f1166855c59ef9e19ceb40
majid2851 <majidbagheri2851@gmail.com>
2023-05-04 13:27:53 +0430

    part 38 ->Done

--- app/src/main/java/com/majid2851/kotlin_modularization/MainActivity.kt
+++ app/src/main/java/com/majid2851/kotlin_modularization/MainActivity.kt
@@ -1,20 +1,28 @@
+@file:OptIn(ExperimentalAnimationApi::class)
+
 package com.majid2851.kotlin_modularization
 
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.animation.core.FastOutSlowInEasing
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.slideInHorizontally
+import androidx.compose.animation.slideOutHorizontally
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.interpolator.view.animation.FastOutSlowInInterpolator
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import coil.ImageLoader
+import com.google.accompanist.navigation.animation.composable
+import com.google.accompanist.navigation.animation.rememberAnimatedNavController
 import com.majid2851.kotlin_modularization.ui.navigation.Screen
 import com.majid2851.kotlin_modularization.ui.theme.DotaInfoTheme
 import com.majid2851.ui_herodetail.ui.HeroDetail
@@ -24,6 +32,7 @@ import com.majid2851.ui_herolist.ui.HeroListViewModel
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
+@OptIn(ExperimentalAnimationApi::class)
 @AndroidEntryPoint
 class MainActivity : ComponentActivity()
 {
@@ -61,7 +70,25 @@ class MainActivity : ComponentActivity()
 fun NavGraphBuilder.addHeroDetail(imageLoader:ImageLoader) {
     composable(
         route = Screen.HeroDetailList.route + "/{heroId}",
-        arguments = Screen.HeroDetailList.arguments
+        arguments = Screen.HeroDetailList.arguments,
+//        enterTransition={_,_ ->
+//            slideInHorizontally (
+//                initialOffsetX = {300},
+//                animationSpec = tween(
+//                    durationMillis = 300,
+//                    easing = FastOutSlowInEasing,
+//                )
+//            ) + fadeIn(animationSpec = tween(300))
+//        },
+//        popExitTransition = {_,_ ->
+//            slideOutHorizontally(
+//                targetOffsetX = {300},
+//                animationSpec = tween(
+//                    durationMillis = 300,
+//                    easing = FastOutSlowInEasing,
+//                )
+//            ) + fadeOut(animationSpec = tween(300))
+//        }
     )
     {
         val viewModel:HeroDetailViewModel = hiltViewModel()
@@ -76,7 +103,27 @@ fun NavGraphBuilder.addHeroList(
     navController: NavHostController,
     imageLoader: ImageLoader
 ) {
-    composable(route = Screen.HeroList.route)
+    composable(
+        route = Screen.HeroList.route,
+//        exitTransition = {_,_->
+//            slideOutHorizontally(
+//                targetOffsetX = {-300},
+//                animationSpec = tween(
+//                    durationMillis = 300,
+//                    easing = FastOutSlowInEasing,
+//                    )
+//                ) + fadeOut(animationSpec = tween(300))
+//        }, popEnterTransition = {_,_ ->
+//            slideInHorizontally (
+//                initialOffsetX = {-300},
+//                animationSpec = tween(
+//                    durationMillis = 300,
+//                    easing = FastOutSlowInEasing,
+//                )
+//            ) + fadeIn(animationSpec = tween(300))
+//        }
+
+    )
     {
         val viewModel: HeroListViewModel = hiltViewModel()
 
--- hero/ui-heroDetail/src/main/java/com/majid2851/ui_herodetail/ui/HeroDetailState.kt
+++ hero/ui-heroDetail/src/main/java/com/majid2851/ui_herodetail/ui/HeroDetailState.kt
@@ -1,11 +1,14 @@
 package com.majid2851.ui_herodetail.ui
 
 import com.majid2851.core.ProgressBarState
+import com.majid2851.core.Queue
+import com.majid2851.core.UIComponent
 import com.majid2851.hero_domain.Hero
 
 data class HeroDetailState(
     val progressBarState:ProgressBarState=ProgressBarState.Idle,
-    val hero: Hero?=null
+    val hero: Hero?=null,
+    val errorQueue: Queue<UIComponent> = Queue(mutableListOf())
 )
 {
 
