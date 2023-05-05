fb655d63b5564da0b822fca32b9dea09134e21a4
majid2851 <majidbagheri2851@gmail.com>
2023-05-05 14:33:32 +0430

    part 44 ->Done

---
+++ hero/hero-datasource-test/src/main/java/com/majid2851/hero_datasource_test/cache/HeroDatabaseFake.kt
@@ -0,0 +1,12 @@
+package com.majid2851.hero_datasource_test.cache
+
+import com.majid2851.hero_domain.Hero
+
+class HeroDatabaseFake ()
+{
+    val heros:MutableList<Hero> = mutableListOf()
+
+
+
+
+}
