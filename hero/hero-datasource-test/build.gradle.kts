3286671116b629dd8f2fef6be88208c5c5ce0cc9
majid2851 <majidbagheri2851@gmail.com>
2023-05-05 15:47:38 +0430

    part 45 ->Done

--- hero/hero-datasource-test/build.gradle.kts
+++ hero/hero-datasource-test/build.gradle.kts
@@ -1,25 +1,20 @@
+
 plugins {
     id("java-library")
     id("org.jetbrains.kotlin.jvm")
+    id(SqlDelight.plugin) version SqlDelight.version
+    kotlin(KotlinPlugins.serialization) version Kotlin.version
 }
 
 java {
     sourceCompatibility = JavaVersion.VERSION_1_7
     targetCompatibility = JavaVersion.VERSION_1_7
 }
-
 dependencies{
-    "implementation"(project(Modules.core))
     "implementation"(project(Modules.heroDataSource))
-
     "implementation"(project(Modules.heroDomain))
-    "implementation"(Kotlinx.coroutinesCore)
-
-
-    "testImplementation"(project(Modules.heroDataSourceTest))
-    "testImplementation"(Junit.junit4)
-    "testImplementation"(Ktor.ktorClientMock)
-    "testImplementation"(Ktor.clientSerialization)
-
-
+    "implementation"(Ktor.ktorClientMock)
+    "implementation"(Ktor.clientSerialization)
 }
+
+
---
+++ hero/hero-interactors/src/test/java/com/majid2851/hero_interactors/GetHerosTest.kt
@@ -0,0 +1,72 @@
+package com.majid2851.hero_interactors
+
+import com.majid2851.core.DataState
+import com.majid2851.core.ProgressBarState
+import com.majid2851.hero_datasource.cache.HeroDatabase
+import com.majid2851.hero_datasource_test.cache.HeroCacheFake
+import com.majid2851.hero_datasource_test.cache.HeroDatabaseFake
+import com.majid2851.hero_datasource_test.network.HeroServiceFake
+import com.majid2851.hero_datasource_test.network.HeroServiceResponseType
+import com.majid2851.hero_datasource_test.network.data.HeroDataValid.NUM_HEROS
+import com.majid2851.hero_domain.Hero
+import kotlinx.coroutines.flow.toList
+import kotlinx.coroutines.runBlocking
+import org.junit.Test
+
+class GetHerosTest
+{
+    private lateinit var getHeros: GetHeros
+
+    @Test
+    fun getHeros_success()
+    {
+        runBlocking {
+            val heroDatabase= HeroDatabaseFake()
+            val heroCache=HeroCacheFake(
+                heroDatabase
+            )
+            val heroService=HeroServiceFake().build(
+                type=HeroServiceResponseType.GoodData
+            )
+
+            getHeros=GetHeros(
+                heroCache = heroCache,
+                service = heroService
+            )
+
+
+            var cachedHeros=heroCache.selectAll()
+            assert(cachedHeros.isEmpty())
+
+
+            val emission=getHeros.excecute().toList()
+
+            assert(emission[0]==DataState.Loading<List<Hero>>(ProgressBarState.Loading))
+
+
+            assert(emission[1] is DataState.Data)
+
+            assert((emission[1] as DataState.Data).data?.size ?: 0 == NUM_HEROS)
+
+            cachedHeros=heroCache.selectAll()
+
+            println("mag2851==================>"+cachedHeros.size.toString())
+            assert(
+                cachedHeros.size== NUM_HEROS
+            )
+
+            assert(emission[2]==DataState.Loading<List<Hero>>(ProgressBarState.Idle))
+
+        }
+
+    }
+
+
+
+
+
+
+
+
+
+}
